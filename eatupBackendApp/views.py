3d5befdab521867d4211a70447b0a84b8a4c658f
Isaac Lim <isaaclimdc@gmail.com>
2013-04-10 22:03:56 -0400

    Changed some field names

--- eatupBackendApp/views.py
+++ eatupBackendApp/views.py
@@ -95,6 +95,7 @@ def getDictArray(reqDict, name):
 
             # split the string into different components
             parts = [p[:-1] for p in rest.split('[')][1:]
+            print parts
             id = int(parts[0])
 
             # add a new dictionary if it doesn't exist yet
